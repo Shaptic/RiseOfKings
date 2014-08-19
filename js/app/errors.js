bc5105d86ce2e40bfb3da962fd263e9855da45ca
Ruskiy69 <zloi.tatarin@yahoo.com>
2014-08-19 13:18:16 -0700

    Adds error message.

--- js/app/errors.js
+++ js/app/errors.js
@@ -1,7 +1,8 @@
 $(function() {
     window.MESSAGES = {
         "CONNECTION_LOST_HOST": $("<span/>").text("Attempting to re-establish a lobby..."),
-        "CONNECTION_LOST":      $("<span/>").text("Connection to lobby lost."),
-        "CONNECTION_LOST_AUTH": $("<span/>").text("Connection to authorization server lost: ")
+        "CONNECTION_LOST_PEER": $("<span/>").addClass("error").text("Lobby lost."),
+        "CONNECTION_LOST":      $("<span/>").addClass("error").text("Connection to lobby lost."),
+        "CONNECTION_LOST_AUTH": $("<span/>").addClass("error").text("Connection to authorization server lost: ")
     }
 });
